923097c0fab88e6343d299c10c0a80802b651198
sandeshsitaula <navaltajohan@gmail.com>
2024-05-01 08:16:08 +0545

    added collection.html for handling all collections

---
+++ scripts/data.js
@@ -0,0 +1,28 @@
+data = [
+  {
+    name: "Beautiful Handmade Vegas Wedding Card",
+    price: "4.49",
+    description:
+      " Beautiful handmade, layered 3D wedding card. Made from sturdy card, with an embossed background, featuring a cute cartoon bride and groom and 5he Las Vegas sign . Perfect for that special new couple who deserves the time and attention that is lovingly put into each handcrafted card. Measuring 5in x 7 in. Inside is left blank for your own message. Each card is accompanied by handmade envelope and is shipped sealed in a cellophane bag for security and protection. Potential for personalisation, please message with requirements and I will do my best to accommodate.",
+    image: "../assets/handmade.avif"
+  },
+  {
+    name: "Beautiful Handmade Vegas Wedding Card",
+    price: "4.49",
+    description:
+      " Beautiful handmade, layered 3D wedding card. Made from sturdy card, with an embossed background, featuring a cute cartoon bride and groom and 5he Las Vegas sign . Perfect for that special new couple who deserves the time and attention that is lovingly put into each handcrafted card. Measuring 5in x 7 in. Inside is left blank for your own message. Each card is accompanied by handmade envelope and is shipped sealed in a cellophane bag for security and protection. Potential for personalisation, please message with requirements and I will do my best to accommodate.",
+    image: "../assets/handmade.avif"
+  },
+  {
+    name: "Beautiful Handmade Vegas Wedding Card",
+    price: "4.49",
+    description:
+      " Beautiful handmade, layered 3D wedding card. Made from sturdy card, with an embossed background, featuring a cute cartoon bride and groom and 5he Las Vegas sign . Perfect for that special new couple who deserves the time and attention that is lovingly put into each handcrafted card. Measuring 5in x 7 in. Inside is left blank for your own message. Each card is accompanied by handmade envelope and is shipped sealed in a cellophane bag for security and protection. Potential for personalisation, please message with requirements and I will do my best to accommodate.",
+    image: "../assets/handmade.avif"
+  }
+];
+
+function returnData() {
+  console.log(data);
+  return data;
+}
