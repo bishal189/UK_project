0628efe24df860fa38a14f607d5d7b14b384793e
sandeshsitaula <navaltajohan@gmail.com>
2024-05-01 11:28:32 +0545

    added productData and product page also proper now

--- scripts/data.js
+++ scripts/data.js
@@ -6,21 +6,35 @@ function returnData() {
     price: "4.49",
     description:
       " Beautiful handmade, layered 3D wedding card. Made from sturdy card, with an embossed background, featuring a cute cartoon bride and groom and 5he Las Vegas sign . Perfect for that special new couple who deserves the time and attention that is lovingly put into each handcrafted card. Measuring 5in x 7 in. Inside is left blank for your own message. Each card is accompanied by handmade envelope and is shipped sealed in a cellophane bag for security and protection. Potential for personalisation, please message with requirements and I will do my best to accommodate.",
-    image: "../assets/handmade.avif"
+        details:[
+        "Handmade","Delivery from a small business in United Kingdom","Materials: cardstock, glue","Delivery cost: £5.55","Returns and exchanges not accepted"
+      ],
+    image: "../assets/handmade.avif",
+    large_image:"../assets/handmade.avif",
   },
   {
     name: "Beautiful Handmade Vegas Wedding Card",
     price: "4.49",
     description:
       " Beautiful handmade, layered 3D wedding card. Made from sturdy card, with an embossed background, featuring a cute cartoon bride and groom and 5he Las Vegas sign . Perfect for that special new couple who deserves the time and attention that is lovingly put into each handcrafted card. Measuring 5in x 7 in. Inside is left blank for your own message. Each card is accompanied by handmade envelope and is shipped sealed in a cellophane bag for security and protection. Potential for personalisation, please message with requirements and I will do my best to accommodate.",
-    image: "../assets/handmade.avif"
+              details:[
+        "Handmade","Delivery from a small business in United Kingdom","Materials: cardstock, glue","Delivery cost: £5.55","Returns and exchanges not accepted"
+      ],
+    image: "../assets/handmade.avif",
+        large_image:"../assets/handmade.avif",
+
   },
   {
     name: "Beautiful Handmade Vegas Wedding Card",
     price: "4.49",
     description:
       " Beautiful handmade, layered 3D wedding card. Made from sturdy card, with an embossed background, featuring a cute cartoon bride and groom and 5he Las Vegas sign . Perfect for that special new couple who deserves the time and attention that is lovingly put into each handcrafted card. Measuring 5in x 7 in. Inside is left blank for your own message. Each card is accompanied by handmade envelope and is shipped sealed in a cellophane bag for security and protection. Potential for personalisation, please message with requirements and I will do my best to accommodate.",
-    image: "../assets/handmade.avif"
+          details:[
+        "Handmade","Delivery from a small business in United Kingdom","Materials: cardstock, glue","Delivery cost: £5.55","Returns and exchanges not accepted"
+      ],
+    image: "../assets/handmade.avif",
+        large_image:"../assets/handmade.avif",
+
   }
 ];
 console.log(data)
